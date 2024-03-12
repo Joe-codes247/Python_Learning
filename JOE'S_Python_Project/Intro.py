c2815504064ecc8d74770c636c46c7912ba487cb
myname <myemail>
2024-03-12 08:36:23 +0300

    My Python lesson

---
+++ JOE'S_Python_Project/Intro.py
@@ -0,0 +1,17 @@
+x,y,z = "PS2", "PS3", "PS4"
+print(x)
+print(y)
+print(z)
+
+elements = ["fire", "air", "water", "earth"]
+w,x,y,z = elements
+print(w)
+print(x)
+print(y)
+print(z)
+
+x = "awesome"
+def myfunc():
+  print("Gaming is " + x)
+
+myfunc()
