13aadfc00e962f0478fd6552f7116c3c7451873c
myname <myemail>
2024-03-26 08:00:51 +0300

    Additional content

--- JOE'S_Python_Project/classes_in_python.py
+++ JOE'S_Python_Project/classes_in_python.py
@@ -8,7 +8,34 @@ class student:
         self.student_age = student_age
 
     def displaystudent(self):
-        print("The student details are:", self.student_name, "Age:", self.student_age)
+        print("The student details are:", "name:" ,self.student_name, "Age:", self.student_age)
 
 student1 = student("100", "Caleb", "20")
 student1.displaystudent()
+
+#Inheritance lesson
+class person:
+    person_name = ""
+    person_residence = ""
+    person_age = 0
+
+    def __init__(them,name,residence,age):
+        them.person_name = name
+        them.person_residence = residence
+        them.person_age = age
+
+    def printTheDetails(them):
+        print("The person details are:",them.person_name,"and Residence:",them.person_residence)
+
+
+
+class Student(person):
+    uniform = ""
+
+    def __init__(them,name,residence,age,uni):
+        super().__init__(name,residence,age)
+        them.uniform = uni
+        print("The student uniform is:",them.uniform)
+
+student1 = Student("Joe","Brazil",20,"Black")
+student1.printTheDetails()
